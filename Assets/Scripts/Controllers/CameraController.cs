6fff32ad318c927338cd81dc2a3306ac94463cab
zzino00 <jinojeong00@gmail.com>
2023-10-26 14:29:41 +0900

    3rd commit - Make camera move towards the player when the wall covers the player

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
@@ -23,9 +24,19 @@ public class CameraController : MonoBehaviour
     {
         if(_mode == Define.CameraMode.QuaterView)
         {
-            transform.position = _player.transform.position + _delta;// _delta는 플레이어위치를 그대로 받으면 플레이어랑 겹치니까 플레이어가 잘보이는 위치
-            transform.LookAt(_player.transform);// 바라보는 방향 설정
+            RaycastHit hit;
+            if(Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))// 플레이어 위치에서 카메라를 향해 빛을 쏘는데 중간에 벽이 있을때만 반환
+            {
+                float dist = (hit.point - _player.transform.position+ new Vector3(0,2.5f,0)).magnitude*0.8f;// 플레이어부터 벽에 빛이 닿은 부분까지의 방향과 크기, 0.8을 곱함으로써 벽에서 살짝 떨어지게 된다.
+                transform.position = _player.transform.position + _delta.normalized*dist;
+            }
+            else
+            {
+                transform.position = _player.transform.position + _delta;// _delta는 플레이어위치를 그대로 받으면 플레이어랑 겹치니까 플레이어가 잘보이는 위치
+                transform.LookAt(_player.transform);// 바라보는 방향 설정
+            }
         }
+           
       
     }
 
