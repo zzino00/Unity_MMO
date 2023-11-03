a267481d8d69bc38384930fe82f472b03613211f
zzino00 <jinojeong00@gmail.com>
2023-11-03 22:36:17 +0900

    7th commit - Bind Events to control with script

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
 
-        Managers.Resource.Instantiate("UI/UI_Button");
+      //  Managers.Resource.Instantiate("UI/UI_Button");
     }
 
    
