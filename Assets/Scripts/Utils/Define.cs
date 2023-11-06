e35f9df4ccbd42b5307d24ca3b9635859b456615
zzino00 <jinojeong00@gmail.com>
2023-11-06 22:58:17 +0900

    11th commit - Working on SoundManager

--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Define 
 {
- public enum Scene
+    public enum Scene
     {
         Unknown,
         Login,
         Lobby,
         Game,
+    }
+
+    public enum Sound
+    {
+        Bgm,
+        Effect,
+        MaxCount,
 
     }
+
     public enum UIEvent
     {
         Click,
